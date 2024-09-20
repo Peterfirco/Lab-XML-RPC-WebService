c2f10f38c0136440d92ecafd14188a573f04e3f1
Admin <Admin@DESKTOP-JOCGTDQ>
2024-09-20 16:27:04 +0300

    Initial Commit.

---
+++ Lab3_XML-RPC_WebService/src/main/java/JavaWebService.java
@@ -0,0 +1,24 @@
+import org.apache.xmlrpc.*;
+
+public class JavaWebService {
+	
+	public int sum(int x, int y)
+	{
+		return x + y;
+	}
+	public static void main (String [] args)
+	{
+		try {
+			System.out.println("Attempting to start XML-RPC Web Service...");
+			WebServer server = new WebServer(8080);
+			server.addHandler("sample", new JavaWebService());
+			server.start();
+			System.out.println("Started successfully.");
+			System.out.println("Accepting requests");
+		}
+		catch (Exception exception)
+		{
+			System.out.println("Java Web service: "+exception);
+		}
+	}
+}
